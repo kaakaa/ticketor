641dcbdbf45f38d78c598eeabd523014cc9c1b04
kaakaa <stooner.hoe@gmail.com>
2015-11-03 00:46:45 +0900

    travis debug

--- test/test.py
+++ test/test.py
@@ -131,6 +131,7 @@ class Test(unittest.TestCase):
 		assert res.status == '302 Found'
 		assert res.headers['Content-Type'] == 'text/html; charset=UTF-8'
 		
+		print os.path.getsize(os.path.join(root, 'test_data', 'create_backlog', 'none.csv'))
 		assert os.path.getsize(os.path.join(root, 'test_data', 'create_backlog', 'none.csv')) == 34
 				
 	def test_burndown(self):
