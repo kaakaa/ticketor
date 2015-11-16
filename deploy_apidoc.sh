a1e773ae4d5210559aa79d0462ff75122f7c5ab7
kaakaa <stooner.hoe@gmail.com>
2015-11-16 19:54:22 +0900

    fix travis

--- deploy_apidoc.sh
+++ deploy_apidoc.sh
@@ -3,17 +3,24 @@ python setup.py build_apidoc
 cd build/sphinx/html
 
 git init
+git config user.email "stooner.hoe@gmail.com"
+git config user.name "kaakaa"
 git add .
 git commit -m "update apidoc"
 
+echo "FETCH GITHUB REPOSITORY"
 git remote add origin https://${GH_TOKEN}github.com/kaakaa/trac-team-task-register.git
 git fetch origin
 
+echo "CHECKOUT GH-PAGES BRANCH"
 git checkout -b gh-pages origin/gh-pages
+echo "MERGE NEW DOCS"
 git merge master
 git checkout --theirs *
 
+echo "COMMIT NEW DOCS"
 git add .
 git commit -m "update apidoc"
 
+echo "PUSH"
 git push -u origin gh-pages
