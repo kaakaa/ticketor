3721a959c4066c5aadcedca1fc337867451e242a
kaakaa <stooner.hoe@gmail.com>
2015-11-16 19:31:17 +0900

    settings of deploying apidoc from travis

---
+++ deploy_apidoc.sh
@@ -0,0 +1,19 @@
+python setup.py build_apidoc
+
+cd build/sphinx/html
+
+git init
+git add .
+git commit -m "update apidoc"
+
+git remote add origin https://github.com/kaakaa/trac-team-task-register.git
+git fetch origin
+
+git checkout -b gh-pages origin/gh-pages
+git merge master
+git checkout --theirs *
+
+git add .
+git commit -m "update apidoc"
+
+git push -u origin gh-pages
