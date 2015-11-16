bb5168c76fbf25315ce6b65c51c80f847669c3a0
kaakaa <stooner.hoe@gmail.com>
2015-11-16 19:45:08 +0900

    fix travis

--- deploy_apidoc.sh
+++ deploy_apidoc.sh
@@ -6,7 +6,7 @@ git init
 git add .
 git commit -m "update apidoc"
 
-git remote add origin https://github.com/kaakaa/trac-team-task-register.git
+git remote add origin https://${GH_TOKEN}github.com/kaakaa/trac-team-task-register.git
 git fetch origin
 
 git checkout -b gh-pages origin/gh-pages
